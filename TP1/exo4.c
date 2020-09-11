2ed8d417b8814879716d0ffddf806193dcc2266c
jeremyL71 <jeremy.laurent97@orange.fr>
2020-09-11 07:38:44 -0700

    fin tp1

--- TP1/exo4.c
+++ TP1/exo4.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <math.h> 
-
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+ 
 int main(int argc, char *argv[]) {
-
-    printf("Nom du programme: %s\n", argv[0]);
-    printf("Nombre d'arguments de la ligne de commande: %d\n", argc-1);
-
-    if(argc=0) perror("Aucune moyenne a calculer");
-
-    /*
-     * On passe le nom de l'exécutable qui est
-     * en première position dans les arguments.
-     */
-    argc--;   argv++;
-
-    int sum = 0;
-    int t = 0;
-
-    /*
-     * On somme toutes les valeurs passées en arguments du programme.
-     * Pour ce faire on déplace le pointeur argv pour scanner tous les arguments.
-     */
-    while ( argc != 0 ) {
-        if(0 >= *argv || 20 <= *argv) perror("Note non valide, pas entre 0 et 20");
-        if(!isdigit(*argv)) perror("Note non valide, ce n'est pas un int");
-
-        printf("\nLa valeur actuelle: %s",*argv);
-        sum += atoi( *argv++ );
-        argc--;
-        t++;
+    int note, total=0;
+    double moyenne;
+    int i,j;
+    int isNumber;
+    int length;
+    char *string;
+ 
+    if (argc <=1) {
+        printf("Aucune moyenne a calculer\n");
+        return 1;
     }
-    
-    printf("\nla somme: %d: \n", sum);
-    float average = sum / (float)t;
-    printf("\nVotre moyenne est: %.2f\n", average);
-    return(0);
-
-} //fin du main
+ 
+    for(i=1;i<argc;i++) {
+        string=argv[i];
+        length = strlen(string);
+        isNumber = 1;
+        for(j=0;j<length;j++) {
+            if (!isdigit(string[j])){
+                isNumber = 0;
+            }
+        }
+        note = atoi(string);
+        if (note>=0 && note<=20 && isNumber){
+            total = total + note;
+        } else {
+            printf("Notes non valide\n");
+            return 1;
+        }
+    }
+ 
+    moyenne = (float)total / (argc-1);
+    printf("Moyenne est : %.2f\n",moyenne);
+    return 0;
+}
--- TP1/exo5.c
+++ TP1/exo5.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+ 
+int main(int argc, char *argv[]) {
+    extern char **environ;
+    int i = 0;
+ 
+    while(environ[i] != NULL){
+        if(commence_par(argv[1],environ[i])){
+            printf("%s\n", environ[i]);
+        }
+        i++;
+    }
+    return 0;
+}
+ 
+int commence_par(char *valeure, char *texte){
+    int i;
+    int return_var=1;
+ 
+    for(i=0;i<strlen(valeure);i++){
+        if(valeure[i]!=texte[i]){
+            return_var=0;
+        }
+    }
+    return return_var;
+}
+
+ /*
+int main(int argc, char *argv[]) {
+    extern char **environ;
+    int i = 0;
+ 
+    while(environ[i] != NULL){
+        if(est_egale(argv[1],environ[i])){
+            printf("%s\n", environ[i]);
+        }
+        i++;
+    }
+    return 0;
+}
+ 
+int est_egale(char *valeur, char *texte){
+    int i=0;
+    int return_var=1;
+ 
+    while(i<strlen(texte) && texte[i]!='='){
+        if(valeur[i]!=texte[i]){
+            return_var=0;
+        }
+        i++;
+    }
+    return return_var;
+}
+
 
 const char* s;
 
@@ -10,7 +64,10 @@ int main(int argc, char *argv[])
     printf("Les arguments sont: ");
     for(int i=1; i<argc ; i++){
         s = getenv(argv[i]);
-        printf("%s :%s\n",argv[i], (s!=NULL)? s : "getenv returned NULL");
+        printf("%s :%s\n",argv[i], (s!=NULL)? s : "getenv a retourner NULL");
     }
     return(0);
 }
+
+
+*/
