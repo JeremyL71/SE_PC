a398af14b47fbed9203853a115dd23608c3f045f
jeremy l <jeremy.laurent@teamwork.net>
2020-11-09 08:44:19 +0000

    exo2

---
+++ TP_python/exo2.py
@@ -0,0 +1,53 @@
+import random
+import names as random_name
+
+
+def exo(N: int):
+    class user(object):
+
+        def __init__(self, name=None, age=None, list_note=None):
+            if name is None:
+                self.name = random_name
+            else:
+                self.name = name
+            if age is None:
+                self.age = generate_random_age()
+            else:
+                self.age = age
+
+            if list_note is None:
+                self.list_note = generate_list_random_note()
+            else:
+                self.list_note = list_note
+
+    def generate_list_random_note():
+        list = []
+        for i in range(5):
+            list.append(random.randint(0, 20))
+        return list
+
+    def generate_random_age():
+        return random.randint(18, 30)
+
+    def create_N_user(N: int):
+        i = 0
+        user_list = []
+        while i < N:
+            user_list.append(
+                user(random_name))
+            i=i+1
+        return user_list
+
+    print("Start main")
+    user_list = create_N_user(N)
+
+    for user in user_list:
+        print(f"user: ", user.name)
+    
+    print("End main")
+
+
+if __name__ == '__main__':
+    # Local test
+    N = 3
+    exo(N)
