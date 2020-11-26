c66b6fe39d3a3c6ec2a0f8a39e67b4c95925ec9f
jeremyL71 <jeremy.laurent97@orange.fr>
2020-11-26 05:12:45 -0800

    correction bug monte carlo'

--- PYTHON_PARA/monte_carlo.py
+++ PYTHON_PARA/monte_carlo.py
@@ -54,21 +54,24 @@ if __name__ == "__main__":
 
     print(f"difference: {abs(temps_multi-temps_mono)}")
 
-    print(f"Debut du temps multiprocess avec: {int(sys.argv[1])} processus")
+    if len(sys.argv) < 2:
+        print("penser a ajout le nom de processus en argument en appelant le programme")
+    else:
+        print(f"Debut du temps multiprocess avec: {int(sys.argv[1])} processus")
 
-    start_time = time.time()
-    # Nummber of points to use for the Pi estimation
-    n = 10000000
-    # iterable with a list of points to generate in each worker
-    # each worker process gets n/np number of points to calculate Pi from
-    part_count = [n / int(sys.argv[1])] * int(sys.argv[1])
-    # Create the worker pool
-    # http://docs.python.org/library/multiprocessing.html#module-multiprocessing.pool
-    pool = Pool(processes=int(sys.argv[1]))
-    # parallel map
-    count = pool.map(monte_carlo_pi_part, part_count)
-    print("Esitmated value of Pi:: ", sum(count) / (n * 1.0) * 4)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    temps_multi = time.time() - start_time
+        start_time = time.time()
+        # Nummber of points to use for the Pi estimation
+        n = 10000000
+        # iterable with a list of points to generate in each worker
+        # each worker process gets n/np number of points to calculate Pi from
+        part_count = [n / int(sys.argv[1])] * int(sys.argv[1])
+        # Create the worker pool
+        # http://docs.python.org/library/multiprocessing.html#module-multiprocessing.pool
+        pool = Pool(processes=int(sys.argv[1]))
+        # parallel map
+        count = pool.map(monte_carlo_pi_part, part_count)
+        print("Esitmated value of Pi:: ", sum(count) / (n * 1.0) * 4)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        temps_multi = time.time() - start_time
 
-    print(f"Fin du temps multiprocess avec: {int(sys.argv[1])} processus")
+        print(f"Fin du temps multiprocess avec: {int(sys.argv[1])} processus")
