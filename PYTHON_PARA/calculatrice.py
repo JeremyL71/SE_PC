39f33995c9a0a9b7d25a625b196bb3c37c29e085
Louis MORANDET <lmorandet@MacBook-Pro-de-Louis.local>
2020-11-26 16:57:34 +0100

    J'aime les calculs

--- PYTHON_PARA/calculatrice.py
+++ PYTHON_PARA/calculatrice.py
@@ -1,42 +1,28 @@
 import os, time, random
+import multiprocessing as mp
 
 
-def fils_calculette(rpipe_commande, wpipe_reponse):
+def fils_calculette(cmd: str,ret: list):
     print(f"Bonjour du Fils: {os.getpid()}")
-
-    while True:
-        cmd = os.read(rpipe_commande, 64)
-        print("Le fils a recu ", cmd)
-        res = eval(cmd)
-        print("Dans fils, le résultat =", res)
-        os.write(wpipe_reponse, str(res).encode())
-        print("Le fils a envoyé", res)
-        time.sleep(1)
-    os._exit(0)
+    print("Le fils a recu ", cmd)
+    res = eval(cmd)
+    print("Dans fils, le résultat =", res)
+    ret[0] = res
+    print("Le fils a envoyé", res)
 
 
 if __name__ == "__main__":
-    rpipe_reponse, wpipe_reponse = os.pipe()
-    rpipe_commande, wpipe_commande = os.pipe()
-
-    pid = os.fork()
-    if pid == 0:
-        fils_calculette(rpipe_commande, wpipe_reponse)
-        assert False, "fork du fils n a pas marché !"# Si échec, on affiche un message
-    else:
-        # On ferme les "portes" non utilisées
-        os.close(wpipe_reponse)
-        os.close(rpipe_commande)
-
-        while True:
-            # Le pere envoie au fils un calcul aléatoire à faire et récupère le résultat
-            opd1 = random.randint(1, 10)
-            opd2 = random.randint(1, 10)
-            operateur = random.choice(["+", "-", "*", "/"])
-            str_commande = str(opd1) + operateur + str(opd2)
-            os.write(wpipe_commande, str_commande.encode())
-            print("Le père va demander à faire : ", str_commande)
-            res = os.read(rpipe_reponse, 32)
-            print("Le Pere a recu ", res)
-            print("-" * 60)
-            time.sleep(1)
+    res = mp.Array("d", 1)
+    while True:
+        # Le pere envoie au fils un calcul aléatoire à faire et récupère le résultat
+        opd1 = random.randint(1, 10)
+        opd2 = random.randint(1, 10)
+        operateur = random.choice(["+", "-", "*", "/"])
+        str_commande = str(opd1) + operateur + str(opd2)
+        fils = mp.Process(target=fils_calculette, args=(str_commande.encode(),res,))
+        fils.start()
+        print("Le père va demander à faire : ", str_commande)
+        fils.join()
+        print("Le Pere a recu ", res[0])
+        print("-" * 60)
+        time.sleep(1)
