58e8652a0ff6f32b00121c07a66f564f559ca006
Louis MORANDET <lmorandet@MacBook-Pro-de-Louis.local>
2020-11-27 10:51:13 +0100

    TP6_update

--- TP6/src/exo2_1.c
+++ TP6/src/exo2_1.c
@@ -6,9 +6,7 @@ int main () {
     int sem1_2 = sem_create(12, 0);
     int sem1_3 = sem_create(13, 0);
     int sem2_1 = sem_create(21, 0);
-    int sem2_3 = sem_create(23, 0);
     int sem3_1 = sem_create(31, 0);
-    int sem3_2 = sem_create(32, 0);
     printf("P1 : T1\n");
     printf("P1 : T2\n");
     printf("P1 : T3\n");
--- TP6/src/exo2_2.c
+++ TP6/src/exo2_2.c
@@ -4,10 +4,8 @@
 
 int main () {
     int sem1_2 = sem_create(12, 0);
-    int sem1_3 = sem_create(13, 0);
     int sem2_1 = sem_create(21, 0);
     int sem2_3 = sem_create(23, 0);
-    int sem3_1 = sem_create(31, 0);
     int sem3_2 = sem_create(32, 0);
     printf("P2 : T1\n");
     printf("P2 : T2\n");
